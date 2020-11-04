80ed475b7acf8743e11511102c122e85786a6e4d
Panos-JC <phatziioannou@gmail.com>
2020-11-04 12:22:27 +0200

    ADD: 404 page

---
+++ src/components/404/NotFound.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Layout } from "../layout/Layout";
+
+interface NotFoundProps {}
+
+export const NotFound: React.FC<NotFoundProps> = ({}) => {
+  return (
+    <Layout maxWidth="sm">
+      <h1>404</h1>
+    </Layout>
+  );
+};
