902f7c071fed2e774a31d50ed95ec716bb7a46da
Panos-JC <phatziioannou@gmail.com>
2020-11-11 16:12:46 +0200

    ADD: stripe

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,13 +3,21 @@ import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import store from "../redux/store";
 import { Routes } from "../router/Routes";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe(
+  "pk_test_51Hhu6gGUOvv7bpMI5MYHR3hYRr0i1RxLT4GxssYJaWY8PAguUCejXB1mlQGO9Z3nGHgCxzDPU1A6IrUWsD0uhwui00jyhvCZrY"
+);
 
 function App() {
   return (
     <Provider store={store}>
-      <Router>
-        <Routes />
-      </Router>
+      <Elements stripe={stripePromise}>
+        <Router>
+          <Routes />
+        </Router>
+      </Elements>
     </Provider>
   );
 }
