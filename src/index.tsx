5f268443046c2f2b67f78821d3191fab0c128a5f
Panos-JC <phatziioannou@gmail.com>
2020-12-01 17:38:20 +0200

    CHANGE: server

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { createMuiTheme, ThemeProvider } from "@material-ui/core";
 
 const client = new ApolloClient({
   // uri: "http://localhost:4000/graphql",
-  uri: "https://fast-scrubland-39510.herokuapp.com/graphql",
+  uri: "https://felea-server-prod.herokuapp.com/graphql",
   cache: new InMemoryCache(),
   credentials: "include",
 });
@@ -23,16 +23,12 @@ const theme = createMuiTheme({
       default: "#f5f5f5",
     },
     primary: {
-      light: "rgba(146, 255, 198, 1)",
-      main: "rgba(92, 219, 149, 1)",
-      dark: "rgba(26, 168, 103, 1)",
+      main: "rgba(24, 43, 85, 1)",
       contrastText: "#fff",
     },
     secondary: {
-      light: "rgba(66, 98, 154, 1)",
-      main: "rgba(5, 56, 107, 1)",
-      dark: "rgba(0, 19, 64, 1)",
-      contrastText: "#fff",
+      main: "rgba(214, 212, 212, 1)",
+      contrastText: "#rgba(24, 43, 85, 1)",
     },
     success: {
       light: "rgba(128, 226, 126, 1)",
@@ -57,41 +53,6 @@ const theme = createMuiTheme({
       hint: "rgba(0, 0, 0, 0.38)",
     },
   },
-
-  // palette: {
-  //   common: {
-  //     black: "#000",
-  //     white: "#fff",
-  //   },
-  //   background: {
-  //     paper: "#fff",
-  //     default: "#fafafa",
-  //   },
-  //   primary: {
-  //     light: "rgba(223, 215, 121, 1)",
-  //     main: "rgba(174, 166, 40, 1)",
-  //     dark: "rgba(121, 114, 12, 1)",
-  //     contrastText: "#fff",
-  //   },
-  //   secondary: {
-  //     light: "rgba(208, 212, 208, 1)",
-  //     main: "rgba(136, 138, 136, 1)",
-  //     dark: "rgba(103, 105, 103, 1)",
-  //     contrastText: "#fff",
-  //   },
-  //   error: {
-  //     light: "#e57373",
-  //     main: "#f44336",
-  //     dark: "#d32f2f",
-  //     contrastText: "#fff",
-  //   },
-  //   text: {
-  //     primary: "rgba(0, 0, 0, 0.87)",
-  //     secondary: "rgba(0, 0, 0, 0.54)",
-  //     disabled: "rgba(0, 0, 0, 0.38)",
-  //     hint: "rgba(0, 0, 0, 0.38)",
-  //   },
-  // },
 });
 
 ReactDOM.render(
