aea8bf4f816c90032c7c8d543c2c4fd9b698cc8a
Panos-JC <phatziioannou@gmail.com>
2021-01-04 17:29:19 +0200

    REMOVE: filters

--- src/components/mentors/Mentors.tsx
+++ src/components/mentors/Mentors.tsx
@@ -1,9 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import { Layout } from "../shared/layout/Layout";
 import { MentorCard } from "./MentorCard";
 import { useMentorsQuery } from "../../generated/graphql";
 import { makeStyles, Grid, Typography } from "@material-ui/core";
-import { SkillFilters } from "./SkillFilters";
 import { Loading } from "../shared/loading/Loading";
 
 const useStyles = makeStyles((theme) => ({
@@ -38,66 +37,69 @@ export const Mentors: React.FC<MentorsProps> = () => {
   const classes = useStyles();
 
   // State
-  const [selectedSkill, setSelectedSkill] = useState<string[]>([]);
-  const [selectedIndustry, setSelectedIndustry] = useState<string[]>([]);
+  // const [selectedSkill, setSelectedSkill] = useState<string[]>([]);
+  // const [selectedIndustry, setSelectedIndustry] = useState<string[]>([]);
 
   // GraphQL
   const { data, loading } = useMentorsQuery({
-    variables: { skills: selectedSkill, industries: selectedIndustry },
+    variables: { skills: [], industries: [] },
   });
 
   // Handle skill filter change
-  const handleSkillChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.value);
+  // const handleSkillChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   console.log(e.target.value);
 
-    const onList = selectedSkill.includes(e.target.value);
+  //   const onList = selectedSkill.includes(e.target.value);
 
-    onList
-      ? setSelectedSkill(
-          selectedSkill.filter((skill) => skill !== e.target.value)
-        )
-      : setSelectedSkill([...selectedSkill, e.target.value]);
-  };
+  //   onList
+  //     ? setSelectedSkill(
+  //         selectedSkill.filter((skill) => skill !== e.target.value)
+  //       )
+  //     : setSelectedSkill([...selectedSkill, e.target.value]);
+  // };
 
   // Handle industry filter change
-  const handleIndustryChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const onList = selectedIndustry.includes(e.target.value);
+  // const handleIndustryChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   const onList = selectedIndustry.includes(e.target.value);
 
-    onList
-      ? setSelectedIndustry(
-          selectedIndustry.filter((industry) => industry !== e.target.value)
-        )
-      : setSelectedIndustry([...selectedIndustry, e.target.value]);
-  };
+  //   onList
+  //     ? setSelectedIndustry(
+  //         selectedIndustry.filter((industry) => industry !== e.target.value)
+  //       )
+  //     : setSelectedIndustry([...selectedIndustry, e.target.value]);
+  // };
+
+  if (loading || !data) {
+    return <Loading />;
+  }
 
   return (
     <Layout maxWidth="lg">
       <Grid container spacing={3}>
         <Grid item xs={3}>
-          <Typography className={classes.title}>Find a mentor</Typography>
+          <Typography className={classes.title}>Our Mentors</Typography>
         </Grid>
       </Grid>
 
       <Grid container spacing={3}>
-        <Grid item xs={3}>
+        {/* <Grid item xs={3}>
           <SkillFilters
             handleSkillChange={handleSkillChange}
             handleIndustryChange={handleIndustryChange}
           />
-        </Grid>
-        <Grid item xs={9}>
-          {loading ? <Loading /> : null}
-          {data &&
-            data.mentors.map((mentor) => (
-              <MentorCard
-                mentor={mentor.mentor}
-                avatar={mentor.mentor.user.avatar}
-                sessions={mentor.sessions}
-                rating={mentor.avg}
-                expertises={mentor.mentor.expertises}
-              />
-            ))}
-        </Grid>
+        </Grid> */}
+
+        {data.mentors.map((mentor) => (
+          <Grid item xs={6}>
+            <MentorCard
+              mentor={mentor.mentor}
+              avatar={mentor.mentor.user.avatar}
+              sessions={mentor.sessions}
+              rating={mentor.avg}
+              expertises={mentor.mentor.expertises}
+            />
+          </Grid>
+        ))}
       </Grid>
     </Layout>
   );
