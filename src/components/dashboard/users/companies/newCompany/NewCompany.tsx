0cef069c7b154b56804bc2ee40fc831b7e2802c1
Panos-JC <phatziioannou@gmail.com>
2021-01-28 11:12:02 +0200

    UPDATE: replace radio btns with text field

--- src/components/dashboard/users/companies/newCompany/NewCompany.tsx
+++ src/components/dashboard/users/companies/newCompany/NewCompany.tsx
@@ -1,16 +1,16 @@
 import {
   Button,
   Card,
-  FormControl,
-  FormControlLabel,
-  FormLabel,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
   makeStyles,
-  Radio,
-  RadioGroup,
   TextField,
 } from "@material-ui/core";
 import React, { useState } from "react";
-import { Controller, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { useHistory } from "react-router-dom";
 import {
   CompaniesDocument,
@@ -21,6 +21,7 @@ import { PageTitle } from "../../../pageTitle/PageTitle";
 const useStyles = makeStyles((theme) => ({
   card: {
     padding: theme.spacing(2),
+    maxWidth: "50%",
   },
   input: {
     width: "100%",
@@ -44,20 +45,24 @@ export const NewCompany: React.FC<NewCompanyProps> = () => {
   const history = useHistory();
 
   // State
-  const [value, setValue] = useState<string>();
   const [error, setError] = useState<string>("");
+  const [open, setOpen] = useState(false);
 
   // GraphQL
   const [createCompany, { loading }] = useCreateCompanyMutation();
 
-  const { register, handleSubmit, control, errors } = useForm<Inputs>();
+  const { register, handleSubmit, errors, getValues } = useForm<Inputs>();
 
-  const onSubmit = async (formData: Inputs) => {
-    console.log(formData);
+  const onSubmit = async () => {
+    setOpen(true);
+  };
+
+  const handleCreate = async () => {
+    const { accounts, name } = getValues();
 
     const { data } = await createCompany({
       variables: {
-        input: { name: formData.name, accounts: parseInt(formData.accounts) },
+        input: { name, accounts: parseInt(accounts) },
       },
       refetchQueries: [{ query: CompaniesDocument }],
     });
@@ -81,32 +86,22 @@ export const NewCompany: React.FC<NewCompanyProps> = () => {
             helperText={errors.name ? "Name is required" : null}
             className={classes.input}
             variant="outlined"
+            placeholder="e.g 10"
             size="small"
             label="Name"
             name="name"
           />
-          <FormControl component="fieldset">
-            <FormLabel component="legend">Number of accounts</FormLabel>
-            <Controller
-              rules={{ required: true }}
-              control={control}
-              defaultValue="2"
-              name="accounts"
-              as={
-                <RadioGroup
-                  aria-label="promoting"
-                  value={value}
-                  onChange={(e) => setValue(e.target.value)}
-                >
-                  <FormControlLabel value="2" control={<Radio />} label="2" />
-                  <FormControlLabel value="3" control={<Radio />} label="3" />
-                  <FormControlLabel value="10" control={<Radio />} label="10" />
-                  <FormControlLabel value="20" control={<Radio />} label="20" />
-                  <FormControlLabel value="30" control={<Radio />} label="30" />
-                </RadioGroup>
-              }
-            />
-          </FormControl>
+          <TextField
+            inputRef={register({ required: true })}
+            error={errors.accounts ? true : false}
+            helperText={errors.accounts ? "Field required" : null}
+            className={classes.input}
+            type="number"
+            label="Number of Accounts"
+            size="small"
+            name="accounts"
+            variant="outlined"
+          />
           <Button
             className={classes.button}
             type="submit"
@@ -119,6 +114,30 @@ export const NewCompany: React.FC<NewCompanyProps> = () => {
           </Button>
         </form>
       </Card>
+      <Dialog
+        open={open}
+        onClose={() => setOpen(false)}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          Confirm Company Creation
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Company name: <strong>{getValues().name}</strong> <br />
+            Number of accounts: <strong>{getValues().accounts}</strong>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpen(false)} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleCreate} color="primary" autoFocus>
+            Confirm
+          </Button>
+        </DialogActions>
+      </Dialog>
     </>
   );
 };
